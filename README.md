4fe543226dffd961e373c2174d4341e3b701aa18
Carles <carlesarnal92@gmail.com>
2020-08-13 13:40:03 +0200

    Add wait and improve Readme

--- README.md
+++ README.md
@@ -1,23 +1,19 @@
 # Apicurio Registry Client Demo
 
+Simple Java application that shows how to use the Apicurio Registry Client.
+
 ## How to run the demo
 
 * Clone and build Apicurio registry project: https://github.com/Apicurio/apicurio-registry
 
 `git clone git@github.com:Apicurio/apicurio-registry.git`
 
-`mvn clean install -DskipTests `
+`mvn clean install -DskipTests -Pasyncmem`
 
-* Run two instances of registry
+* Run one instance of registry
 
 `java -jar -Dquarkus.profile=dev /home/carles/IdeaProjects/apicurio-registry/storage/asyncmem/target/apicurio-registry-storage-asyncmem-1.2.3-SNAPSHOT-runner.jar`
 
-`java -jar -Dquarkus.profile=dev -Dquarkus.http.port=8081 /home/carles/IdeaProjects/apicurio-registry/storage/asyncmem/target/apicurio-registry-storage-asyncmem-1.2.3-SNAPSHOT-runner.jar`
-
 * Run demo's Main (from IDE)
 
-`https://github.com/alesj/registry-demo/blob/master/src/main/java/io/apicurio/registry/demo/Main.java`
-
-* Run demo's TestMain (from IDE)
-
-`https://github.com/alesj/registry-demo/blob/master/src/test/java/io/apicurio/registry/test/TestMain.java`
+`https://github.com/carlesarnal/apicurio-registry-client-demo/blob/master/src/main/java/io/apicurio/registry/client/demo/RegistryDemoApp.java`
--- src/main/java/io/apicurio/registry/client/demo/RegistryDemoApp.java
+++ src/main/java/io/apicurio/registry/client/demo/RegistryDemoApp.java
@@ -1,14 +1,13 @@
 package io.apicurio.registry.client.demo;
 
+import io.apicurio.registry.client.SimpleRegistryClient;
+import io.apicurio.registry.client.SimpleRegistryService;
 import io.apicurio.registry.rest.beans.ArtifactMetaData;
 import io.apicurio.registry.rest.beans.IfExistsType;
 import io.apicurio.registry.types.ArtifactType;
-import io.registry.client.RegistryClient;
-import io.registry.client.RegistryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.WebApplicationException;
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
@@ -16,11 +15,10 @@ import java.util.UUID;
 
 /**
  * Simple demo app that shows how to use the client.
- *
+ * <p>
  * 1) Register a new schema in the Registry.
  * 2) Fetch the newly created schema.
  * 3) Delete the schema.
- *
  *
  * @author Carles Arnal <carnalca@redhat.com>
  */
@@ -28,41 +26,29 @@ public class RegistryDemoApp {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RegistryDemoApp.class);
 
-    private static final RegistryService service;
+    private static final SimpleRegistryService service;
 
     static {
         // Create a Service Registry client
-        String registryUrl = "http://localhost:8080/api";
-        service = RegistryClient.create(registryUrl);
+        String registryUrl = "http://localhost:8080/api/";
+        service = SimpleRegistryClient.create(registryUrl);
     }
 
     public static void main(String[] args) throws Exception {
-        try {
-            LOGGER.info("\n\n--------------\nBootstrapping the JSON Schema demo.\n--------------\n");
 
-            // Register the JSON Schema schema in the Apicurio registry.
+        // Register the JSON Schema schema in the Apicurio registry.
+        String artifactId = UUID.randomUUID().toString();
 
-            String artifactId = UUID.randomUUID().toString();
+        createSchemaInServiceRegistry(artifactId, Constants.SCHEMA);
 
-            try {
-                createSchemaInServiceRegistry(artifactId, Constants.SCHEMA);
+        //Wait for the artifact to be available.
+        Thread.sleep(1000);
 
-                getSchemaFromRegistry(artifactId);
+        getSchemaFromRegistry(artifactId);
 
-                deleteSchema(artifactId);
+        deleteSchema(artifactId);
 
-            } catch (Exception e) {
-                if (is409Error(e)) {
-                    LOGGER.warn("\n\n--------------\nWARNING: Schema already existed in registry!\n--------------\n");
-                    return;
-                } else {
-                    throw e;
-                }
-            }
-
-            LOGGER.info("\n\n--------------\nBootstrapping complete.\n--------------\n");
-        } finally {
-        }
+        System.exit(0);
     }
 
     /**
@@ -70,9 +56,8 @@ public class RegistryDemoApp {
      *
      * @param artifactId
      * @param schema
-     * @throws Exception
      */
-    private static void createSchemaInServiceRegistry(String artifactId, String schema) throws Exception {
+    private static void createSchemaInServiceRegistry(String artifactId, String schema) {
 
         LOGGER.info("---------------------------------------------------------");
         LOGGER.info("=====> Creating artifact in the registry for JSON Schema with ID: {}", artifactId);
@@ -91,9 +76,8 @@ public class RegistryDemoApp {
      * Get the artifact from the registry.
      *
      * @param artifactId
-     * @throws Exception
      */
-    private static ArtifactMetaData getSchemaFromRegistry(String artifactId) throws Exception {
+    private static ArtifactMetaData getSchemaFromRegistry(String artifactId) {
 
         LOGGER.info("---------------------------------------------------------");
         LOGGER.info("=====> Fetching artifact from the registry for JSON Schema with ID: {}", artifactId);
@@ -112,28 +96,17 @@ public class RegistryDemoApp {
      * Delete the artifact from the registry.
      *
      * @param artifactId
-     * @throws Exception
      */
-    private static void deleteSchema(String artifactId) throws Exception {
+    private static void deleteSchema(String artifactId) {
 
         LOGGER.info("---------------------------------------------------------");
         LOGGER.info("=====> Deleting artifact from the registry for JSON Schema with ID: {}", artifactId);
         try {
-             service.deleteArtifact(artifactId);
+            service.deleteArtifact(artifactId);
             LOGGER.info("=====> Successfully deleted JSON Schema artifact in Service Registry.");
             LOGGER.info("---------------------------------------------------------");
         } catch (Exception t) {
             throw t;
         }
-    }
-
-    private static boolean is409Error(Exception e) {
-        if (e.getCause() instanceof WebApplicationException) {
-            WebApplicationException wae = (WebApplicationException) e.getCause();
-            if (wae.getResponse().getStatus() == 409) {
-                return true;
-            }
-        }
-        return false;
     }
 }
